c870c3c4694ecd24aef058b74de005205210b737
Robettaja <rompula2005@gmail.com>
2026-01-30 22:45:15 +0200

    päivitetty conffia

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,20 +1,22 @@
 return {
-  {
-    "kylechui/nvim-surround",
-    opts = {
+  "kylechui/nvim-surround",
+  version = "*",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
       keymaps = {
-        normal = {
-          add = "ns", -- Add surrounding in normal mode
-          delete = "nd", -- Delete surrounding
-          change = "nc", -- Change surrounding
-        },
-        visual = {
-          add = "ns", -- Add surrounding in visual mode
-        },
-        insert = {
-          add = "ns", -- Insert mode add
-        },
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
       },
-    },
-  },
+    })
+  end,
 }
